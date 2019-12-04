1a06adf8631f37a4f0495f34adb22f0422a6e1b1
djayeng <57972661+djayeng@users.noreply.github.com>
2019-12-04 23:09:22 +0700

    Update and rename dj.php to lea.php

--- lea.php
+++ lea.php
@@ -79,14 +79,14 @@ echo " ===========================\n";
         goto goride;
         }else{
         echo "\n".color("red","-] Message: ".$messageboba10);
-        echo "\n".color("yellow","!] Claim voc GFSPESIAL20K");
+        echo "\n".color("yellow","!] Claim voc GOFOODSANTAI11");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
         sleep(1);
         }
         sleep(3);
-        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GFSPESIAL20K"}');
+        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI11"}');
         $messageboba19 = fetch_value($boba19,'"message":"','"');
         if(strpos($boba19, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba19);
